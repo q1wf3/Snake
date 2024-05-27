e60feeb00f0468dbe9de7f4f1c7b31c2f9244a37
q1wf3 <151677823+q1wf3@users.noreply.github.com>
2024-05-27 03:21:58 +0300

    Snake.cpp

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -2,25 +2,27 @@
 
 #include "Snake.h"
 
-Snake::Snake(int width, int height, int maxLength) {
-    max_length = maxLength;
-    length = 1;
-    array_X = new int[max_length];
+Snake::Snake(int width, int height, int maxLength) { // Инициализирует массивы координат и устанавливает начальную длину и позицию змейки
+    max_length = maxLength; // Устанавливает максимальную длину змейки
+    length = 1; // Начальная длина змейки 
+    array_X = new int[max_length]; // Создание массива для хранения координат x сегментов змейки
     array_Y = new int[max_length];
-    array_X[0] = width / 2;
+    array_X[0] = width / 2; // Установка начальной координаты x головы змейки в центре поля
     array_Y[0] = height / 2;
 }
+// Деструктор класса Snake
 
+// Освобождает память, выделенную для массивов координат змейки
 Snake::~Snake() {
-    delete[] array_X;
+    delete[] array_X; // Удаление массива координат x
     delete[] array_Y;
 }
 
-int* Snake::getX() {
+int* Snake::getX() { // Метод для получения массива координат x сегментов змейки
     return array_X;
 }
 
-int* Snake::getY() {
+int* Snake::getY() { // Метод для получения текущей длины змейки
     return array_Y;
 }
 
@@ -28,7 +30,7 @@ int Snake::getLength() {
     return length;
 }
 
-void Snake::increaseLength(int x, int y) {
+void Snake::increaseLength(int x, int y) { // Метод для увеличения длины змейки и добавления нового сегмента
     if (length < max_length) {
         length++;
         array_X[length - 1] = x;
