fc4213c5cd320ecf6abf84f86259b0da6a401994
q1wf3 <151677823+q1wf3@users.noreply.github.com>
2024-05-27 03:20:16 +0300

    Apple.cpp

--- Snake/Apple.cpp
+++ Snake/Apple.cpp
@@ -3,15 +3,15 @@
 #include "Apple.h"
 #include <cstdlib>
 
-Apple::Apple(int width, int height) {
+Apple::Apple(int width, int height) { // Инициализирует координаты яблока случайными значениями внутри границ игрового поля
     x = rand() % (width - 2) + 1;
     y = rand() % (height - 2) + 1;
 }
 
-int Apple::getX() {
+int Apple::getX() { // Метод для получения координаты x яблока
     return x;
 }
 
-int Apple::getY() {
+int Apple::getY() { // Метод для получения координаты y яблока
     return y;
 }
